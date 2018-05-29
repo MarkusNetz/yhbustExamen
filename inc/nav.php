31a5d75d31f1b8d8fe478dd3d81f8b9f7761fb97
Markus Netz <markus.netz.89@gmail.com>
2018-05-29 16:27:32 +0200

    Commiting small changens to nav.php

--- inc/nav.php
+++ inc/nav.php
@@ -1,17 +1,23 @@
 <!-- W3.CSS -->
-<div class="w3-bar w3-black w3-mobile">
-	<a href="/" class="w3-bar-item w3-button">Home</a>
-	<a href="/profile" class="w3-bar-item w3-button">Profiles</a>
+<div class="w3-bar w3-black w3-mobile w3-row">
+	<a href="/" class="w3-bar-item w3-button w3-block w3-col s12 m4 l1">Home</a>
+	
+	<a href="/profile" class="w3-bar-item w3-button w3-block w3-col s12 m4 l1">Konto</a>
+	
+	<a href="/profile" class="w3-bar-item w3-button w3-block w3-col s12 m4 l1">Presentation</a>
+	
 	<div class="w3-dropdown-hover">
-		<button class="w3-button">Dropdown</button>
+		<button class="w3-button">CV</button>
 		<div class="w3-dropdown-content w3-bar-block w3-card-4">
-			<a href="#" class="w3-bar-item w3-button">Link 1</a>
-			<a href="#" class="w3-bar-item w3-button">Link 2</a>
-			<a href="#" class="w3-bar-item w3-button">Link 3</a>
+			<a href="#" class="w3-bar-item w3-button w3-col s12 m4 l1">CV-hantering</a>
+			<a href="#" class="w3-bar-item w3-button w3-col s12 m4 l1">Dela CV</a>
+			<a href="#" class="w3-bar-item w3-button w3-col s12 m4 l1">Link 3</a>
 		</div>
 	</div>
-	<a href="/login" class="w3-bar-item w3-button">Sign in</a>
+	
+	<a href="/login" class="w3-bar-item w3-button w3-block w3-col s12 m4 l1">Sign in</a>
+	
 	<div class="float-right">
-		<input type="text" class="w3-bar-item w3-input" placeholder="Search.."><a href="#" class="w3-bar-item w3-button w3-theme-l2">Go</a>
+		<input type="text" class="w3-bar-item w3-input " placeholder="Search.."><a href="#" class="w3-bar-item w3-button w3-theme-l2"><i class="fa fa-search"></i></a>
 	</div>
 </div>
